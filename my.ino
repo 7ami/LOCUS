7571671ae1cba0125acf09d4de831ff4b0e76b31
7ami <074bct504.amir@pcampus.edu.np>
2019-02-03 04:50:58 -0800

    disatnce 20

--- my.ino
+++ my.ino
@@ -1,95 +1,105 @@
-int S2= 6; 
-int S3= 7; 
-int outPin = 8; 
-int rColorStrength; 
-int gColorStrength; 
-int bColorStrength; 
-unsigned int pulseWidth;
+#include <Servo.h>
+Servo servo1;
+int buzzer=10;
+int trigPin = 12;
+int echoPin = 13;
+int servoPin=3;
+void object(int,int);
+int color();
+long unsigned int duration;
+float distance;
+float dist();
+void disp(float,int);
+float dis;
+int i;
+void buzz();
+const int s[4]={4,5,6,7};
+const int sensOut=8;
+int frequency;
+int red();
+int blue();int green();
 void setup() {
-  
-  Serial.begin(115200);   
-  pinMode(S2, OUTPUT); 
-  pinMode(S3, OUTPUT); 
-  pinMode(outPin, INPUT); 
-  }
-
-void loop() {
-  
-
-  digitalWrite(S2, LOW);
-  digitalWrite(S3, LOW);
-  
-  pulseWidth =  pulseIn(outPin, LOW); 
-  
-  rColorStrength = pulseWidth/400. -1; 
-  
-  rColorStrength = (255- rColorStrength); 
-  digitalWrite(S2, HIGH);
-  digitalWrite(S3, HIGH);
-  
-  pulseWidth =  pulseIn(outPin, LOW); 
-  
-  gColorStrength = pulseWidth/400. -1;   
-  gColorStrength = (255- gColorStrength);
-  
-  gColorStrength=gColorStrength + 2;
- 
-  digitalWrite(S2, LOW);
-  digitalWrite(S3, HIGH);
-  
-  pulseWidth =  pulseIn(outPin, LOW); 
-  
-  bColorStrength = pulseWidth/400. -1;
-  
-  bColorStrength = (255- bColorStrength);
-  
- if(rColorStrength>gColorStrength && gColorStrength>bColorStrength) {
-   
-   rColorStrength = 255;
-   gColorStrength = gColorStrength/2;
-   bColorStrength = 0;
- }
- 
-  if(rColorStrength>bColorStrength && bColorStrength>gColorStrength) {
-   
-   rColorStrength = 255;
-   bColorStrength = bColorStrength/2;
-   gColorStrength = 0;
- }
- 
-  if(gColorStrength>rColorStrength && rColorStrength>bColorStrength) {
-   
-   gColorStrength = 255;
-   rColorStrength = rColorStrength/2;
-   bColorStrength = 0;
- }
- 
-   if(gColorStrength>bColorStrength && bColorStrength>rColorStrength) {
-   
-   gColorStrength = 255;
-   bColorStrength = bColorStrength/2;
-   rColorStrength = 0;
- }
- 
-   if(bColorStrength>rColorStrength && rColorStrength>gColorStrength) {
-   
-   bColorStrength = 255;
-   rColorStrength = rColorStrength/2;
-   gColorStrength = 0;
- }
- 
-    if(bColorStrength>gColorStrength && gColorStrength>rColorStrength) {
-   
-   bColorStrength = 255;
-   gColorStrength = gColorStrength/2;
-   rColorStrength = 0;
- }
- 
-  Serial.print(rColorStrength);
-  Serial.print(",");
-  Serial.print(gColorStrength);
-  Serial.print(",");
-  Serial.println(bColorStrength);
-  delay(2000);
-  
+  // put your setup code here, to run once
+  Serial.begin(9600);
+  pinMode(trigPin,OUTPUT);
+  pinMode(echoPin,INPUT);
+  servo1.attach(servoPin);
+  //digitalWrite(servoPin,HIGH);
+  servo1.write(30);
+  pinMode(buzzer,OUTPUT);
+  pinMode(s[0],OUTPUT);
+  pinMode(s[1],OUTPUT);
+  pinMode(s[2],OUTPUT);
+  pinMode(s[3],OUTPUT);
+  pinMode(sensOut,INPUT);
+  pinMode(s[0],HIGH);
+  pinMode(s[1],LOW);
 }
+void loop() {
+  for (i=5;i<175;i+=3)
+  {
+    servo1.write(i);
+    dis=dist();
+    if (dis<20) {disp(dis,i);}
+    
+    delay(200);
+  }
+  for(i=175;i>5;i-=3)
+  {
+    servo1.write(i);
+    dis=dist();
+    if (dis<20) {disp(dis,i);}
+    
+    delay(200);
+  }
+}
+float dist()
+  {
+  digitalWrite(trigPin,LOW);
+  delay(10);
+  digitalWrite(trigPin,HIGH);
+  delay(10);
+  digitalWrite(trigPin,LOW);
+  duration=pulseIn(echoPin,HIGH);
+  distance=(duration*330)/20000;
+  return distance;
+  }
+  void disp(float d,int p)
+  {
+    
+    Serial.print(d);
+    Serial.print(",");
+    Serial.print(red());
+    Serial.print(",");
+    Serial.print(green());
+    Serial.print(",");
+    Serial.println(blue());
+    buzz();
+  }
+  void buzz()
+  {
+   tone(buzzer,1000);
+   delay(100);
+   noTone(buzzer);
+  }
+  int red()
+  {
+    digitalWrite(s[2],LOW);
+    digitalWrite(s[3],LOW);
+    frequency = pulseIn(sensOut, LOW);
+    return frequency;
+  }
+  int green()
+  {
+    digitalWrite(s[2],HIGH);
+    digitalWrite(s[3],HIGH);
+    frequency = pulseIn(sensOut, LOW);
+    return frequency;
+  }
+  int blue()
+  {
+    digitalWrite(s[2],LOW);
+    digitalWrite(s[3],HIGH);
+    frequency = pulseIn(sensOut, LOW);
+    return frequency;
+  }
